d07b3d3772b34657a5b359bce6ce81b59bb060a6
Boris Ivanov <boris.iv.iv@gmail.com>
2017-05-29 00:30:56 +0300

    New drivers.

--- fresh-ubuntu.sh
+++ fresh-ubuntu.sh
@@ -1,74 +1,78 @@
 # fresh-ubuntu.sh
 #-------------------------------------------------------
-echo "Click on the same icon will minimise the window."
+function echoRed() {
+	echo -e "\e[91m$1\e[0m"
+}
+
+echoRed "Click on the same icon will minimise the window."
 gsettings set org.compiz.unityshell:/org/compiz/profiles/unity/plugins/unityshell/ launcher-minimize-window true
 
-echo "Enable Local applciation menu."
+echoRed "Enable Local applciation menu."
 gsettings set com.canonical.Unity integrated-menus true
 
-echo "Always show menu."
+echoRed "Always show menu."
 gsettings set com.canonical.Unity always-show-menus true
 
-echo "Stop sending search terms from Dash to hird party companies."
+echoRed "Stop sending search terms from Dash to hird party companies."
 gsettings set com.canonical.Unity.ApplicationsLens display-available-apps false
 
-# echo "Move Unity luncher to the bottom (Ubuntu 16.04 only)"
+# echoRed "Move Unity luncher to the bottom (Ubuntu 16.04 only)"
 # gsettings set com.canonical.Unity.Launcher launcher-position Bottom
 
-echo "Checking for updates..."
-sudo apt-get update
-sudo apt-get upgrade
-sudo apt-get dist-upgrade
+echoRed "Checking for updates..."
+sudo apt-get -y update
+sudo apt-get -y upgrade
+sudo apt-get -y dist-upgrade
 
-echo "Installing unity-tweak-tool..."
-sudo apt-get install unity-tweak-tool
+echoRed "Installing unity-tweak-tool..."
+sudo apt-get -y install unity-tweak-tool
 
-echo "Installing Sublime Text 3..."
+echoRed "Installing Sublime Text 3..."
 sudo add-apt-repository ppa:webupd8team/sublime-text-3
-sudo apt-get update
-sudo apt-get install sublime-text-installer
+sudo apt-get -y update
+sudo apt-get -y install sublime-text-installer
 
-echo "Installing vim..." 
-sudo apt-get install vim
+echoRed "Installing vim..." 
+sudo apt-get -y install vim
 
-echo "Installing python-pygments and creating alias ccat for syntax highlighted cat..." 
-sudo apt-get install python-pygments
-echo -e "\nalias ccat='pygmentize -g'" >> ~/.bash_aliases 
+echoRed "Installing python-pygments and creating alias ccat for syntax highlighted cat..." 
+sudo apt-get -y install python-pygments
+echo -e "\nalias ccat='pygmentize -g'" >> ~/.bash_aliases
 
-echo "Installing wine..." 
+echoRed "Installing VLC Player..."
+sudo apt-get -y install vlc 
+
+echoRed "Installing GIMP..."
+sudo apt-get -y install gimp gimp-data gimp-plugin-registry gimp-data-extras
+
+echoRed "Installing Flash Player..."
+sudo apt-get -y install flashplugin-installer 
+
+echoRed "Installing ubuntu-restricted-extras..."
+sudo apt-get -y install ubuntu-restricted-extras 
+
+echoRed "Installing wine..." 
 sudo apt-get install wine 
 
-echo "Installing VLC Player..."
-sudo apt-get install vlc 
-
-echo "Installing GIMP..."
-sudo apt-get install gimp gimp-data gimp-plugin-registry gimp-data-extras
-
-echo "Installing Flash Player..."
-sudo apt-get install flashplugin-installer 
-
-echo "Installing ubuntu-restricted-extras..."
-sudo apt-get install ubuntu-restricted-extras 
-
-echo "Installing nVidia drivers..."
-if lspci | grep -i vga | grep -i nvidia; then
-	sudo apt-get purge nvidia-*
+echoRed "Installing nVidia drivers..."
+if lspci | grep -i nvidia; then
 	sudo add-apt-repository ppa:graphics-drivers/ppa
-	sudo apt-get update
-	sudo apt-get install nvidia-378 nvidia-settings nvidia-prime prime-indicator
+	sudo apt-get -y update
+	sudo apt-get -y purge nvidia-*
+	sudo apt-get -y install nvidia-381 nvidia-settings nvidia-prime prime-indicator
 	nvidiaInstalled=yes
 else 
 	echo "No nVidia card found." >&2
 fi
 
-echo "Cleaning up..." &&
+echoRed "Cleaning up..." &&
 sudo apt-get -f install &&
 sudo apt-get autoremove &&
 sudo apt-get -y autoclean &&
 sudo apt-get -y clean
 
 if [[ $nvidiaInstalled == yes ]]; then
-	echo "Rebooting..."
+	echoRed "Rebooting..."
 	sudo reboot 
 fi
 #-------------------------------------------------------
